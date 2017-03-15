33faf807f84e2e62433b1b9c40ef4bebfca6614f
astrolka <astrolka@rambler.ru>
2017-03-15 21:11:49 +0300

    Initial Commit

---
+++ StepsTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StepsTest
+//
+//  Created by Александр Смоленский on 15.03.17.
+//  Copyright © 2017 Александр Смоленский. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ StepsTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  StepsTest
+//
+//  Created by Александр Смоленский on 15.03.17.
+//  Copyright © 2017 Александр Смоленский. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
